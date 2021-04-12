fdf2fb562cb0f1bdd993adbbc3cec48102c217b9
Just4tune <s247920@studenti.polito.it>
2021-04-12 12:49:35 +0200

    prova commit

---
+++ LAB1/es1.py
@@ -0,0 +1,13 @@
+
+print("inserire valore 1:")
+val1=int(input())
+print("Inserire valore 2:")
+val2= int(input())
+
+prod= val1 * val2
+
+if prod>1000:
+
+    print("la loro somma è:",val1+val2)
+else:
+    print("Il loro prodotto è:", prod)
